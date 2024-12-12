e1ca0187b8a6c8469fccab8ddc752e1e28ee24a1
Stefan Nygren <stefan.nygren@scania.com>
2024-12-12 19:08:25 +0100

    2024-12

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -17,6 +17,7 @@
 #include "Day09.h"
 #include "Day10.h"
 #include "Day11.h"
+#include "Day12.h"
 //DayHeader
 
 struct DayInfo {
@@ -45,6 +46,7 @@ std::vector<DayInfo> createDays() {
     days.push_back({std::make_unique<Day09>(), "09"});
     days.push_back({std::make_unique<Day10>(), "10"});
     days.push_back({std::make_unique<Day11>(), "11"});
+    days.push_back({std::make_unique<Day12>(), "12"});
     //dayCreator
     return days;
 }
